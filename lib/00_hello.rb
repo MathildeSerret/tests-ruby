1845b6c3dfa08e6561d13153328c8dc91eea0c46
MathildeSerret <55735934+MathildeSerret@users.noreply.github.com>
2019-10-07 12:28:16 +0200

    Update 00_hello.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,18 +1,7 @@
 def hello
- 	puts "Hello!" 
  	return "Hello!"	
 end
 
-def greet
-	puts "Quel est ton nom ?"
-	user_name = gets.chomp
-	print "Hello", " ", user_name, " ", "!"
-	return "Hello", " ", user_name, " ", "!"
+def greet(name)
+	return "Hello #{name}"
 end
-
-def perform
-	hello
-	greet
-end
-
-perform
